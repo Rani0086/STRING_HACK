7638a15d847ebf1e7c1103174acce0dd4a369e31
Rani0086 <shantidhaakad@gmail.com>
2024-11-06 21:48:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 class Config:
     API_ID = "27079591"
     API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
-    #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
+    #TOKEN = "7837097346:AAGmI0BfMnHjd9FzdZo6ovVTxjF4QnrQhTM"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://telegra.ph/file/c53d9a7df9dbaa5f4db05.jpg"
-    SUDOERS = filters.user(["6730956183"])
+    MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
+    START_PIC = "https://envs.sh/jbP.png"
+    SUDOERS = filters.user(["7526369190"])
